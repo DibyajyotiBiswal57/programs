8759c78b913c9782c34af1239f9a5cb72b0ede5b
Dibyajyoti <dibbispersonal@gmail.com>
2026-01-11 13:32:10 +0530

    Create fibonacci.py

---
+++ python/fibonacci.py
@@ -0,0 +1,10 @@
+# Fibonacci series up to n terms
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+# Example: print first 10 Fibonacci numbers
+fibonacci(10)
