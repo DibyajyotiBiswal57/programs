d358384a6e3860c4244377b07e59f77288a92465
Dibyajyoti <dibbispersonal@gmail.com>
2026-01-19 22:57:59 +0530

    c

---
+++ python/Upper_or_lower.py
@@ -0,0 +1,12 @@
+#Q48
+
+str = input("Enter a character: ")
+if len(str) > 1:
+    print("Aborting program. Character expected, string found.")
+else:
+    if str == str.upper():
+        print(f"{str} is uppercase.")
+    elif str == str.lower():
+        print(f"{str} is lowercase.")
+    else:
+        print("Invalid charcter. Please try again.")
