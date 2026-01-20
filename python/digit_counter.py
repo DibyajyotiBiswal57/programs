e80dec375bceb8a543074221d1745051fa0a593b
Dibyajyoti <dibbispersonal@gmail.com>
2026-01-20 19:06:27 +0530

    c

---
+++ python/digit_counter.py
@@ -0,0 +1,11 @@
+#Q62
+num = abs(int(input("Enter a number: ")))
+count = 0
+
+if num == 0:
+    count = 1
+else:
+    while num > 0:
+        num = num // 10
+        count += 1
+print("Number of digits:", count)
---
+++ python/quit_loop.py
@@ -0,0 +1,7 @@
+#Q61
+exit = str(0)
+i = 1
+while i > 0:
+    exit = input("Enter 'quit' to exit: ")
+    if exit == 'quit':
+        break
