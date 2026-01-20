9656b013496c7f9f010020d3bab570f427dcf3ab
Dibyajyoti <dibbispersonal@gmail.com>
2026-01-20 21:59:36 +0530

    Update and rename div_by_7_or_3.py to 28_div_by_7_or_3.py

--- python/28_div_by_7_or_3.py
+++ python/28_div_by_7_or_3.py
@@ -1,7 +1,8 @@
-#Q29
+#Q28
 print("This program check if a number is divisible by 7 or 3")
 num=float(input("Enter a number: "))
 if num%3==0 and num%7==0:
     print(f"{num} is divisible by 7 and 3.")
 else:
     print(f"{num} is not divisible by 7 and 3.")
+
