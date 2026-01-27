8028e15e5e7605ac92e659de555ecb13c730fb96
Dibyajyoti <dibbispersonal@gmail.com>
2026-01-27 15:52:07 +0530

    Updated cube_or_square.java with new content on 2026-01-27

---
+++ java/cube_or_square.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class cube_or_square
+{
+    public static void main(String args[])
+    {
+        System.out.println("This program lets you find the square or cube of a number."); 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to find the cube or square?");
+        System.out.println("For cube press'C'");
+        System.out.println("For square press'S'");
+        String a = scanner.nextLine();
+        double sqr;
+        double cube;
+        if(a=="S")
+        {
+        System.out.println("Enter the number:");
+        double input = scanner.nextDouble();
+        sqr=input*input;
+        System.out.println("The answer is: "+sqr);
+    }
+        else if(a=="C")
+        {
+        System.out.println("Enter the number:");
+        double input = scanner.nextDouble();
+        cube=input*input*input;
+        System.out.println("The answer is: "+cube);
+    }
+        scanner.close();
+    }
+}
