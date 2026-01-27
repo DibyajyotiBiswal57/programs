11556af74b4ef56560b86b7257d7677f19510dca
Dibyajyoti <dibbispersonal@gmail.com>
2026-01-27 17:09:37 +0530

    Create 0020_prime_check.py

---
+++ python/0020_prime_check.py
@@ -0,0 +1,13 @@
+#Q60
+count = 0
+i = 1
+num = int(input("Enter a number: "))
+while i <= num:
+    if num % i == 0:
+        count += 1
+    i +=1
+if count ==2:
+    print("The number is prime.")
+else:
+    print("the number is composite.")
+print(count)
