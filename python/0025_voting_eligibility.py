4f62cf7d5586988aa98f1d176d49749dea90897d
Dibyajyoti Biswal <dibbispersonal@gmail.com>
2026-02-07 18:03:04 +0530

    Delete python/0025_voting_eligibility.py
    Signed-off-by: Dibyajyoti Biswal <dibbispersonal@gmail.com>

--- python/0025_voting_eligibility.py
+++
@@ -1,7 +0,0 @@
-# Q25
-print("This program checks if a person is eligible to vote.")
-age = int(input("Enter your age: "))
-if age >= 18:
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote.")
